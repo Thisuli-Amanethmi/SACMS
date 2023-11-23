1a487038a66c7fba4fb53ea096bdfeb54e2f556b
NinaAbeyratne <ninadilkushi@gmail.com>
2023-11-24 04:29:38 +0530

    club advisor register FXML file created and updated. ClubAdvisorController updated

--- src/main/java/com/example/sacms_grp30/ClubAdvisorRegisterController.java
+++ src/main/java/com/example/sacms_grp30/ClubAdvisorRegisterController.java
@@ -1,4 +1,23 @@
 package com.example.sacms_grp30;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ClubAdvisorRegisterController {
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    void loadHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerClubAdvisor(ActionEvent event) {
+
+    }
 }
