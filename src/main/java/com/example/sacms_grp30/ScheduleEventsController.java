118e008ae390ae4594f5b19919401bc4718ad721
Rushdi-Rzx <rushdimohideen@gmail.com>
2023-11-28 23:18:23 +0530

    Event, Meeting Scheduling and Event,Meeting Viewing done. Need to Comment and connect to the database

--- src/main/java/com/example/sacms_grp30/ScheduleEventsController.java
+++ src/main/java/com/example/sacms_grp30/ScheduleEventsController.java
@@ -1,27 +1,25 @@
 package com.example.sacms_grp30;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 public class ScheduleEventsController {
     @FXML
     private Button BackButton;
     @FXML
-    private Button FindButton;
-    @FXML
     private Button SubmitButton;
     @FXML
     private Button ClearButton;
     @FXML
-    private TextField ClubName;
+    private ChoiceBox EventID;
     @FXML
     private TextField EventName;
     @FXML
@@ -35,14 +33,79 @@ public class ScheduleEventsController {
     @FXML
     private Label SuccessfulMessage;
 
+    @FXML
+    public void initialize() {
+        ObservableList<String> eventIDs = FXCollections.observableArrayList("EID001", "EID002", "EID003", "EID004", "EID005", "EID006", "EID007", "EID008");
+        EventID.setItems(eventIDs);
+    }
 
-    public void handleBackButton() throws Exception{
+
+    public void handleBackButton() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("EventSchedulingScene.fxml"));
         Stage window = (Stage) BackButton.getScene().getWindow();
-        window.setScene(new Scene(root,800,500));
+        window.setScene(new Scene(root, 800, 500));
     }
-    public void handleSubmitButton(ActionEvent event) throws Exception{
-        SuccessfulMessage.setText("EVENT SCHEDULED SUCCESSFULLY");
+
+    public void addEventDetails() {
+        ArrayList event = new ArrayList();
+
+        event.add(EventID.getValue().toString());
+        event.add(EventName.getText());
+        event.add(EventDate.getValue().toString());
+        event.add(EventTime.getText());
+        event.add(EventLocation.getText());
+        event.add(EventDescription.getText());
+        Events.eventDetailsList.add(event);
+        SuccessfulMessage.setText("Event Scheduled Successfully");
+        clearTextFields();
+    }
+
+    String existingEvent;
+
+    public void handleSubmitButton(ActionEvent event) throws Exception {
+        if (EventID.getValue() == null && EventName.getText().isEmpty() && EventDate.getValue() == null && EventTime.getText().isEmpty() && EventLocation.getText().isEmpty() && EventDescription.getText().isEmpty()) {
+            SuccessfulMessage.setText("All the fields are Empty. Please Fill!");
+        } else if (EventID.getValue() == null || EventName.getText().isEmpty() || EventDate.getValue() == null || EventTime.getText().isEmpty() || EventLocation.getText().isEmpty() || EventDescription.getText().isEmpty()) {
+            SuccessfulMessage.setText("");
+            SuccessfulMessage.setText("Please Fill All the fields!");
+        } else if (!(EventTime.getText().isEmpty() || EventLocation.getText().isEmpty())) {
+            try {
+                if (existingEntries((String) EventID.getValue())) {
+                    SuccessfulMessage.setText("Event ID Already Taken. Please chose another one.");
+                } else {
+                    addEventDetails();
+                }
+            } catch (Exception exception) {
+
+            }
+        }
+    }
+    public void handleClearButton(ActionEvent event) throws Exception {
+        clearTextFields();
+        SuccessfulMessage.setText("");
+    }
+
+    private void clearTextFields() {
+        EventID.setValue(null);
+        EventName.clear();
+        EventDate.setValue(null);
+        EventTime.clear();
+        EventLocation.clear();
+        EventDescription.clear();
+    }
+
+    public boolean existingEntries(String existingID) {
+        boolean idExisting = false;
+        for (int i = 0; i < Events.eventDetailsList.size(); i++) {
+            if (existingID.equals(Events.eventDetailsList.get(i).get(0))) {
+
+                existingEvent = Events.eventDetailsList.get(i).get(0) + ", " + Events.eventDetailsList.get(i).get(1) + ", " + Events.eventDetailsList.get(i).get(2) + ", " + Events.eventDetailsList.get(i).get(3) + ", " + Events.eventDetailsList.get(i).get(4) + ", " + Events.eventDetailsList.get(i).get(5);
+                idExisting = true;
+            }
+        }
+        return idExisting;
     }
 
 }
+
+
--- src/main/java/com/example/sacms_grp30/ScheduledMeetings.java
+++ src/main/java/com/example/sacms_grp30/ScheduledMeetings.java
@@ -1,4 +1,66 @@
 package com.example.sacms_grp30;
 
 public class ScheduledMeetings {
+    private String meetingId;
+    private String meetingTopic;
+    private String meetingDate;
+    private String meetingTime;
+    private String meetingPlatform;
+    private String meetingDescription;
+
+    public ScheduledMeetings(String meetingId, String meetingTopic, String meetingDate, String meetingTime, String meetingPlatform, String meetingDescription) {
+        this.meetingId = meetingId;
+        this.meetingTopic = meetingTopic;
+        this.meetingDate = meetingDate;
+        this.meetingTime = meetingTime;
+        this.meetingPlatform = meetingPlatform;
+        this.meetingDescription = meetingDescription;
+    }
+    public String getMeetingId() {
+        return meetingId;
+    }
+
+    public void setMeetingId(String meetingId) {
+        this.meetingId = meetingId;
+    }
+
+    public String getMeetingTopic() {
+        return meetingTopic;
+    }
+
+    public void setMeetingTopic(String meetingTopic) {
+        this.meetingTopic = meetingTopic;
+    }
+
+    public String getMeetingDate() {
+        return meetingDate;
+    }
+
+    public void setMeetingDate(String meetingDate) {
+        this.meetingDate = meetingDate;
+    }
+
+    public String getMeetingTime() {
+        return meetingTime;
+    }
+
+    public void setMeetingTime(String meetingTime) {
+        this.meetingTime = meetingTime;
+    }
+
+    public String getMeetingPlatform() {
+        return meetingPlatform;
+    }
+
+    public void setMeetingPlatform(String meetingPlatform) {
+        this.meetingPlatform = meetingPlatform;
+    }
+
+    public String getMeetingDescription() {
+        return meetingDescription;
+    }
+
+    public void setMeetingDescription(String meetingDescription) {
+        this.meetingDescription = meetingDescription;
+    }
 }
