7497388407e19cfa38bf8185157c164197a1e76f
Thisuli Amanethmi <124507196+Thisuli-Amanethmi@users.noreply.github.com>
2023-11-29 08:23:54 +0530

    Student view clubs, events and meetings

--- src/main/java/com/example/sacms_grp30/StudentViewClubViewController.java
+++ src/main/java/com/example/sacms_grp30/StudentViewClubViewController.java
@@ -1,29 +1,47 @@
 package com.example.sacms_grp30;
 
+import com.example.sacms_grp30.db.DBConnection;
+import com.example.sacms_grp30.model.ClubSearchModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-public class StudentViewClubViewController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StudentViewClubViewController implements Initializable {
     @FXML
-    private TableView clubDetailsTable;
+    private TableView<ClubSearchModel> clubDetailsTable;
 
     @FXML
-    private TableColumn clubIDColumn;
+    private TableColumn<ClubSearchModel, String> clubIDColumn;
 
     @FXML
-    private TableColumn clubNameColumn;
+    private TableColumn<ClubSearchModel, String> clubNameColumn;
 
     @FXML
-    private TableColumn clubCategoryColumn;
+    private TableColumn<ClubSearchModel, String> clubCategoryColumn;
 
     @FXML
-    private TableColumn clubDescriptionColumn;
+    private TableColumn<ClubSearchModel, String> clubDescriptionColumn;
 
     @FXML
     private TextField clubNameJoinTxt;
@@ -39,6 +57,75 @@ public class StudentViewClubViewController {
 
     @FXML
     private TextField searchKeywordsTxt;
+
+    ObservableList<ClubSearchModel> clubSearchModelObservableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resource) { // initializable interface
+        Connection connection = DBConnection.getInstance().getConnection(); // to use the created Connection in DBConnection class
+        String clubSearchQuery = "SELECT club_id, club_name, club_category, description FROM club;"; // sql query
+
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(clubSearchQuery);
+
+            while (rs.next()) {
+                String clubID = rs.getString("club_id");
+                String clubName = rs.getString("club_name");
+                String clubCategory = rs.getString("club_category");
+                String clubDescription = rs.getString("description");
+
+                // populate the observableList
+                clubSearchModelObservableList.add(new ClubSearchModel(clubID, clubName, clubCategory, clubDescription));
+            }
+
+            // PropertyValueFactory corresponds to the new clubSearchModel fields
+            // The table column is, the one we annotate above
+            clubIDColumn.setCellValueFactory(new PropertyValueFactory<>("club_id"));
+            clubNameColumn.setCellValueFactory(new PropertyValueFactory<>("club_name"));
+            clubCategoryColumn.setCellValueFactory(new PropertyValueFactory<>("club_category"));
+            clubDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+            clubDetailsTable.setItems(clubSearchModelObservableList);
+
+            // initial filtered list
+            FilteredList<ClubSearchModel> filteredData = new FilteredList<>(clubSearchModelObservableList, b -> true);
+
+            searchKeywordsTxt.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(clubSearchModel -> {
+                    // if no value, then display all records or whatever records it currently have. no changes
+                    if(newValue.isEmpty() || newValue.isBlank() || newValue == null) {
+                     return true;
+                    }
+
+                    String searchKeyword = newValue.toLowerCase();
+
+                    if(clubSearchModel.getClub_id().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club_id
+                    } else if(clubSearchModel.getClub_name().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club_name
+                    } else if (clubSearchModel.getClub_category().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club category
+                    } else if (clubSearchModel.getDescription().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club description
+                    } else {
+                        return false; // no match found
+                    }
+                });
+            });
+
+            SortedList<ClubSearchModel> sortedData = new SortedList<>(filteredData);
+
+            // bind sorted result with the table view
+            sortedData.comparatorProperty().bind(clubDetailsTable.comparatorProperty());
+
+            // apply filtered and sorted data to the table view
+            clubDetailsTable.setItems(sortedData);
+
+        } catch(SQLException e) {
+            Logger.getLogger(StudentViewClubViewController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 
     public void backButtonOnClick(ActionEvent actionEvent) throws Exception {
         navigateBack(actionEvent);
--- src/main/java/com/example/sacms_grp30/StudentViewEventsViewController.java
+++ src/main/java/com/example/sacms_grp30/StudentViewEventsViewController.java
@@ -1,38 +1,56 @@
 package com.example.sacms_grp30;
 
+import com.example.sacms_grp30.db.DBConnection;
+import com.example.sacms_grp30.model.ClubSearchModel;
+import com.example.sacms_grp30.model.EventSearchModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-public class StudentViewEventsViewController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StudentViewEventsViewController implements Initializable {
     @FXML
     private Button registerEventButton;
 
     @FXML
-    private TableView eventDetailsTable;
+    private TableView<EventSearchModel> eventDetailsTable;
 
     @FXML
-    private TableColumn eventIDColumn;
+    private TableColumn<EventSearchModel, String> eventIDColumn;
 
     @FXML
-    private TableColumn eventNameColumn;
+    private TableColumn<EventSearchModel, String> eventNameColumn;
 
     @FXML
-    private TableColumn clubNameColumn;
+    private TableColumn<EventSearchModel, String> eventDateColumn;
 
     @FXML
-    private TableColumn eventDateColumn;
+    private TableColumn<EventSearchModel, String> eventTimeColumn;
 
     @FXML
-    private TableColumn eventTimeColumn;
+    private TableColumn<EventSearchModel, String> eventLocationColumn;
 
     @FXML
-    private TableColumn eventLocationColumn;
+    private TableColumn<EventSearchModel, String> eventDescriptionColumn;
 
     @FXML
     private TextField eventNameRegisterTxt;
@@ -45,6 +63,83 @@ public class StudentViewEventsViewController {
 
     @FXML
     private Button backButton;
+
+    ObservableList<EventSearchModel> eventSearchModelObservableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resource) { // initializable interface
+        Connection connection = DBConnection.getInstance().getConnection(); // to use the created Connection in DBConnection class
+        String clubSearchQuery = "SELECT event_id, event_name, event_date, event_time, event_location, event_description FROM events"; // sql query
+
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(clubSearchQuery);
+
+            while (rs.next()) {
+                String eventID = rs.getString("event_id");
+                String eventName = rs.getString("event_name");
+                String eventDate = rs.getString("event_date");
+                String eventTime = rs.getString("event_time");
+                String eventLocation = rs.getString("event_location");
+                String eventDescription = rs.getString("event_description");
+
+                // populate the observableList
+                eventSearchModelObservableList.add(new EventSearchModel(eventID, eventName, eventDate, eventTime, eventLocation, eventDescription));
+            }
+
+            // PropertyValueFactory corresponds to the new clubSearchModel fields
+            // The table column is, the one we annotate above
+            eventIDColumn.setCellValueFactory(new PropertyValueFactory<>("event_id"));
+            eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("event_name"));
+            eventDateColumn.setCellValueFactory(new PropertyValueFactory<>("event_date"));
+            eventTimeColumn.setCellValueFactory(new PropertyValueFactory<>("event_time"));
+            eventLocationColumn.setCellValueFactory(new PropertyValueFactory<>("event_location"));
+            eventDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("event_description"));
+
+            eventDetailsTable.setItems(eventSearchModelObservableList);
+
+            // initial filtered list
+            FilteredList<EventSearchModel> filteredData = new FilteredList<>(eventSearchModelObservableList, b -> true);
+
+            searchKeywordsTxt.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(eventSearchModel -> {
+                    // if no value, then display all records or whatever records it currently have. no changes
+                    if(newValue.isEmpty() || newValue.isBlank() || newValue == null) {
+                        return true;
+                    }
+
+                    String searchKeyword = newValue.toLowerCase();
+
+                    if(eventSearchModel.getEvent_id().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club_id
+                    } else if(eventSearchModel.getEvent_name().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club_name
+                    } else if (eventSearchModel.getEvent_date().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club category
+                    } else if (eventSearchModel.getEvent_time().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club description
+                    } else if (eventSearchModel.getEvent_location().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club category
+                    } else if (eventSearchModel.getEvent_description().toLowerCase().indexOf(searchKeyword) > -1) {
+                        return true; // means we found a match club description
+                    } else {
+                        return false; // no match found
+                    }
+                });
+            });
+
+            SortedList<EventSearchModel> sortedData = new SortedList<>(filteredData);
+
+            // bind sorted result with the table view
+            sortedData.comparatorProperty().bind(eventDetailsTable.comparatorProperty());
+
+            // apply filtered and sorted data to the table view
+            eventDetailsTable.setItems(sortedData);
+
+        } catch(SQLException e) {
+            Logger.getLogger(StudentViewClubViewController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 
 
     public void backButtonOnClick(ActionEvent actionEvent) throws Exception {
