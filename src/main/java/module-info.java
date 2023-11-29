65b53fd436608f57cd38281e72d07059a1726148
NinaAbeyratne <150835212+NinaAbeyratne@users.noreply.github.com>
2023-11-29 16:15:30 +0530

    mysql connnector added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.cwsample {
     requires javafx.fxml;
     requires java.sql;
     requires java.logging;
-
+    requires mysql.connector.j;
 
 
     exports com.example.sacms_grp30;
